b0d1197d52321f1ce39a2fc7a6d598abfbfb44b2
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-03-02 13:52:56 +0000

    general styles

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import './navbar.css';
 import profile2 from '../../assets/profile2.jpeg';
 
+const data = [
+    { id: 1, link: '#', title: 'Home' },
+    { id: 2, link: '#about', title: 'About' },
+    { id: 3, link: '#projects', title: 'Projects' },
+    { id: 4, link: '#contact', title: 'Contact' }
+]
+
 const navbar = () => {
     return (
-        <nav id='navbar'></nav>
+        <nav>
+            <div className="container nav-container">
+                <a href="index.hmtl" className="ProfilePic">
+                    <img src={profile2} alt="profile pic" />
+                </a>
+                <ul className="nav-menu"> {
+                    data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+                }
+                </ul>
+            </div>
+        </nav>
     )
 }
+
 
 export default navbar
